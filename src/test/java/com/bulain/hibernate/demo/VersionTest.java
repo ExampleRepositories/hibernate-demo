64005c400d3182cd97a8176bd91dd556afae315f
Bulain Peng <bulain@gmail.com>
2012-01-13 16:58:43 +0800

    change version test case

--- src/test/java/com/bulain/hibernate/demo/VersionTest.java
+++ src/test/java/com/bulain/hibernate/demo/VersionTest.java
@@ -1,8 +1,10 @@
 package com.bulain.hibernate.demo;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.StaleObjectStateException;
 import org.hibernate.Transaction;
 import org.hibernate.classic.Session;
 import org.junit.Test;
@@ -11,38 +13,51 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.bulain.common.test.SpringTestCase;
 import com.bulain.hibernate.entity.Person;
 
-public class VersionTest extends SpringTestCase{
+public class VersionTest extends SpringTestCase {
     @Autowired
     private SessionFactory sessionFactory;
     
     @Test
     public void testVersion() {
-        Session session = sessionFactory.openSession();
         Person person = new Person();
         person.setFirstName("firstName");
         person.setLastName("lastName");
+        Session session = sessionFactory.openSession();
         Transaction tx = session.beginTransaction();
         session.save(person);
         tx.commit();
+        session.close();
         Integer version = person.getVersion();
         assertEquals(Integer.valueOf(0), version);
         
+        session = sessionFactory.openSession();
         person = (Person) session.get(Person.class, person.getId());
+        session.close();
         person.setFirstName("firstName-updated");
         person.setLastName("lastName-updated");
+        session = sessionFactory.openSession();
         tx = session.beginTransaction();
         session.update(person);
         tx.commit();
+        session.close();
         version = person.getVersion();
         assertEquals(Integer.valueOf(1), version);
         
+        session = sessionFactory.openSession();
         person = (Person) session.get(Person.class, person.getId());
+        session.close();
         person.setFirstName("firstName-again");
         person.setLastName("lastName-again");
+        person.setVersion(100);
+        session = sessionFactory.openSession();
         tx = session.beginTransaction();
-        session.update(person);
-        tx.commit();
-        version = person.getVersion();
-        assertEquals(Integer.valueOf(2), version);
+        try {
+            session.update(person);
+            tx.commit();
+            fail("Optimistic lock check fail");
+        } catch (StaleObjectStateException e) {
+            tx.rollback();
+        }
+        session.close();
     }
 }
